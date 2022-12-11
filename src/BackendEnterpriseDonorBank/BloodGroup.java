9dfde896be1600f14fec9adcc4af1d2e596fad2b
vidishaShetty12 <114865815+vidishaShetty12@users.noreply.github.com>
2022-12-10 20:55:15 -0500

    donor bank

---
+++ src/BackendEnterpriseDonorBank/BloodGroup.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BackendEnterpriseDonorBank;
+
+/**
+ *
+ * @author asus
+ * This Class holds all the functionalities for the Blood Group 
+ */
+public class BloodGroup {
+    
+    private String bloodg;
+
+    public BloodGroup() {
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public String getBloodg() {
+        return bloodg;
+    }
+
+    public void setBloodg(String bloodg) {
+        this.bloodg = bloodg;
+    }
+}
---
+++ src/BackendEnterpriseDonorBank/OrganName.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BackendEnterpriseDonorBank;
+
+/**
+ *
+ * @author asus
+ 
+ */
+public class OrganName {
+    
+    private String organ;
+    
+    public OrganName() {
+        this.organ = organ;
+    }
+    
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    public String getOrgan() {
+        return organ;
+    }    
+}
